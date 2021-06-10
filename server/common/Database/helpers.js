313e16e3f34a3e957cfb5223e17504e7497733df
Lorena Braghini <lorenabraghini@usp.br>
2021-06-09 22:36:06 -0300

    aaa

--- server/common/Database/helpers.js
+++ server/common/Database/helpers.js
@@ -1,41 +1,45 @@
-const sql = require('mssql')
+const sql = require("mssql");
 
 const sqlConfig = {
-    user: "BD",
-    password: "epbancodedados",
-    database: "Streaming",
-    server: 'localhost',
-    port: 1433,
-    pool: {
-      max: 10,
-      min: 0,
-      idleTimeoutMillis: 30000
-    },
-    options: {
-      encrypt: false, // for azure
-      trustServerCertificate: true // change to true for local dev / self-signed certs
-    }
-  }
+  user: "BD",
+  password: "epbancodedados",
+  database: "Streaming",
+  server: "localhost",
+  port: 1433,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+  },
+};
 
-async function conectar () {
-    const x = await sql.connect(sqlConfig)
-    console.log(x)
+async function conectar() {
+  const x = await sql.connect(sqlConfig);
+  console.log(x);
 }
 //conectar()
 
-function addMusica(musica){
-    return new Promise(async (resolve, reject) => {
-        try {
-            const db = await sql.connect(sqlConfig)
-            await db.request().query(`INSERT INTO [Musica] VALUES (${musica.id}, '${musica.nome}', '${musica.url}', ${musica.duracao}, ${musica.popularidade}, '${musica.genero}')`)
-            resolve()
-        } catch (error) {
-            reject(error)
-        }
-    })
+function addMusica(musica) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const db = await sql.connect(sqlConfig);
+      await db
+        .request()
+        .query(
+          `INSERT INTO [Musica] VALUES (${musica.id}, '${musica.nome}', '${musica.url}', ${musica.duracao}, ${musica.popularidade}, '${musica.genero}')`
+        );
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
-async function teste(){
-    await addMusica({})
+async function teste() {
+  await addMusica({});
 }
 
-teste()
+teste();
